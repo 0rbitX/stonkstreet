d982e89f2c5c7fbadc2ed497052962124a595310
Orbit X <153139012+0rbitX@users.noreply.github.com>
2025-01-10 03:51:50 +0000

    commit

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js"; 
-import { getFirestore, doc, setDoc, getDoc, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
 // Your Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAAL6ZU9cRMKKd4IbTzqdjkyJar0zu1kP8",
@@ -9,12 +8,40 @@ const firebaseConfig = {
   messagingSenderId: "719359222053",
   appId: "1:719359222053:web:327ae56869f46ba5217acc"
 };
-
+firebase.initializeApp(firebaseConfig);
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db=getFirestore(app)
-async function editDB(db,){}
+const db = firebase.firestore();
+function addDB(coll,){}
 
+$("#hi").on("click",function(){
+  const stonks = db.collection("all").doc("stonks")
+  stonks.get().then((doc)=>{
+    if(doc.exists){
+      var y= doc.data()
+      console.log(y.list[0].curprice)
+    }
+  })
+})
+$("#bye").on("click",function(){
+  var rncache;
+  var cost;
+  var nlist;
+  const stonks = db.collection("all").doc("stonks")
+  stonks.get().then((doc)=>{
+    if(doc.exists){
+      var y = doc.data()
+     localStorage.setItem("bye",doc.data())
+     console.log(doc.data())
+     cost = y.list;
+     console.log(cost)
+       cost[0].curprice = 13;
+       stonks.update({
+         list:cost,
+       })
+    }
+  })
+
+})
 function w3_open() {
     document.getElementById("main").style.marginLeft = "25%";
     document.getElementById("mySidebar").style.width = "25%";
@@ -28,7 +55,12 @@ function w3_open() {
   }
   const myChart = new Chart("myChart", {
     type: "line",
-    data: {labels:["hi",1],datasets:[{data:[12,13]}]},
+    data: {labels:["hi",1,2],datasets:[{data:[12,13,3]}]},
+    options: {}
+  });
+  const myChar = new Chart("myChar", {
+    type: "line",
+    data: {labels:["hi",1,2],datasets:[{data:[12,13,3]}]},
     options: {}
   });
   
